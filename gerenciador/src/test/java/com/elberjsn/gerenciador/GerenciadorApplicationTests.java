f50296feea1f12ff1465c6555bbcff8e949f8d88
elberjsn <elberjsn@gmail.com>
2024-11-25 12:11:12 -0300

    inicialização da aplicação

---
+++ gerenciador/src/test/java/com/elberjsn/gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elberjsn.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
